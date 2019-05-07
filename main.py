f8866c0afd279acc4ae278322e664cb36ee3a184
Omar Quraishi <oquraishi1@live.maryville.edu>
2019-05-07 17:15:25 -0500

    Week 1

---
+++ main.py
@@ -0,0 +1,21 @@
+#Import Statements for Program
+
+#Cprint Package Import
+from cprint import cprint
+
+#Bloody Terminal Package Import
+from bloodyterminal import btext
+
+#User Input
+fullName= str(input("Enter your full name: "))
+
+#Combine user input and sentence
+sentence = fullName + " watch this sentence flash in green which is excellent way to debug a program!"
+
+#Print Statements in the Terminal
+
+#Success Message
+cprint(sentence, c='gF')
+
+#Custom Message
+btext.custom("This is a custom alert message", "You have reached the end of the program. Thanks!")
